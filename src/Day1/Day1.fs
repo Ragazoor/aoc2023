54eb3dbe7a4fbf1608695c6e63a474e522c63d21
Ragnar Englund <ragnar.englund@annotell.com>
2023-12-19 20:05:10 +0100

    refactor structure

---
+++ src/Day1/Day1.fs
@@ -0,0 +1,24 @@
+module Day1
+
+open System
+
+let getLines filePath =
+  System.IO.File.ReadAllLines(filePath)
+  |> Seq.toList
+
+let input = getLines "Day1/input.txt"
+let removeLetters str = str |> String.filter System.Char.IsDigit
+
+let calibrationValue (str: string) =
+  let first: string = str.[0].ToString()
+  let last: string = str.[str.Length - 1].ToString()
+  int (first + last)
+
+let getCalibrationValue1 input = input |> removeLetters |> calibrationValue
+
+let part1 =
+  (input |> List.sumBy getCalibrationValue1)
+
+
+
+
