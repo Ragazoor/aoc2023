d729a87708e6bcd7b1c4c636701029529e42c956
Ragnar Englund <ragnar.englund@annotell.com>
2023-12-19 20:45:02 +0100

    new structure

--- src/AoC2023/Day1/Day1.fs
+++ src/AoC2023/Day1/Day1.fs
@@ -6,7 +6,7 @@ let getLines filePath =
   System.IO.File.ReadAllLines(filePath)
   |> Seq.toList
 
-let input = getLines "src/Day1/input.txt"
+let input = getLines "src/AoC2023/Day1/input.txt"
 let removeLetters str = str |> String.filter System.Char.IsDigit
 
 let calibrationValue (str: string) =
