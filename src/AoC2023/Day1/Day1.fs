5590f70aabde872725ba7757d8b08c12462b89ab
Ragnar Englund <ragnar.englund@annotell.com>
2023-12-20 18:57:26 +0100

    day 1 part 2 solved

--- src/AoC2023/Day1/Day1.fs
+++ src/AoC2023/Day1/Day1.fs
@@ -10,8 +10,7 @@ module Day1 =
 
   let private getCalibrationValue1 input = input |> removeLetters |> calibrationValue
 
-  let part1 input =
-    input |> List.sumBy getCalibrationValue1
+  let part1 input = input |> List.sumBy getCalibrationValue1
 
   let private lookup = Map [
           ("1","1"); ("2","2"); ("3","3"); ("4","4"); ("5","5"); ("6","6"); ("7","7"); ("8","8");
@@ -19,8 +18,13 @@ module Day1 =
           ("six","6"); ("seven","7"); ("eight","8"); ("nine","9")
       ]
 
-  let private getNumberLine line: string =
-    lookup.Keys |> Seq.fold (fun acc key -> acc.Replace(key, lookup[key])) line
+  let private getNumberLine (line: string) =
+    let strArray  = Array.create line.Length ""
+    for key in lookup.Keys do
+      let index = line.IndexOf(key)
+      if index >= 0 then
+        strArray.[index] <- lookup.[key]
+    strArray |> Array.map string |> Array.reduce (+)
 
   let private getCalibrationValue2 input = input |> getNumberLine |> removeLetters |> calibrationValue
   let part2 input = input |> List.sumBy getCalibrationValue2
