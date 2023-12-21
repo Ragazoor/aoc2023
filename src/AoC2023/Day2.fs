9d396037256be42130c7230717232e0d14b9d082
Ragnar Englund <ragnar.englund@annotell.com>
2023-12-21 23:02:58 +0100

    day2 passes test

--- src/AoC2023/Day2.fs
+++ src/AoC2023/Day2.fs
@@ -3,10 +3,42 @@ open AoC2023.Common
 
 module Day2 =
 
+  type private Cubes = {red: int; green: int; blue: int}
+  type private Game = {id: int; cubes: Cubes seq}
+  let private isImpossibleGame cube =
+    cube.red > 12 || cube.green > 13 || cube.blue > 14
+  let private getIdIfValidGame game =
+      if Seq.exists isImpossibleGame game.cubes then
+        None
+      else
+        Some game.id
+
   let firstRegex = System.Text.RegularExpressions.Regex(@"Game (\d+):(?:((?:(?: \d+ \w+),)*(?: \d+ \w+))(?:;?))+")
   let secondRegex = System.Text.RegularExpressions.Regex(@"(?: (?:(\d+) (\w+)),?)+")
-  let private getGameResult input =
-    0
-  let private part1 input = input |> List.sumBy getGameResult
+  let private parseGroup group =
+      let matches = secondRegex.Matches(group)
+      let countSeq = matches.[0].Groups.[1].Captures |> Seq.map (fun c -> c.Value |> int)
+      let colorSeq = matches.[0].Groups.[2].Captures |> Seq.map (fun c -> c.Value)
+      let colorMap = Map.ofSeq (Seq.zip colorSeq countSeq)
+      let red = Map.tryFind "red" colorMap |> Option.defaultValue 0
+      let green = Map.tryFind "green" colorMap |> Option.defaultValue 0
+      let blue = Map.tryFind "blue" colorMap |> Option.defaultValue 0
+      {red = red; green = green; blue = blue}
+  let private parseGame line =
+    let matches = firstRegex.Matches(line)
+    let gameNumber = matches.[0].Groups.[1].Captures.[0].Value |> int
+    let cubes =
+      matches.[0].Groups.[2].Captures
+      |> Seq.map (fun c -> c.Value)
+      |> Seq.map parseGroup
+    {id = gameNumber; cubes = cubes}
+
+  let private getGameResult (gameStr: string) =
+    gameStr
+    |> parseGame
+    |> getIdIfValidGame
+    |> Option.defaultValue 0
+
+  let part1 input = input |> List.sumBy getGameResult
 
   let solver = { part1 = part1; part2 = part1}
--- tests/AoC2023.Test/Day2Tests.fs
+++ tests/AoC2023.Test/Day2Tests.fs
@@ -7,7 +7,6 @@ open AoC2023
 let ``Test regex`` () =
   let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
   let matches = Day2.firstRegex.Matches(input)
-  matches |> Seq.iter (fun m -> printfn "%A" m)
 
   let group1 = " 3 blue, 4 red"
   let group2 = " 1 red, 2 green, 6 blue"
@@ -27,13 +26,13 @@ let ``Test regex`` () =
   Assert.Equal("blue", Day2.secondRegex.Matches(group2).[0].Groups[2].Captures[2].Value )
 
 
-//[<Fact>]
-//let ``Part 1`` () =
-//  let input = [
-//    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
-//    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue";
-//    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
-//    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red";
-//    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-//    ]
-//  Assert.Equal(8, Day2.part1 input)
+[<Fact>]
+let ``Part 1`` () =
+  let input = [
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue";
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red";
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+    ]
+  Assert.Equal(8, Day2.part1 input)
