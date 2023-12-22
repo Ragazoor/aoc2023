a97859c0d3ea465e527dafdee8b9c6c57a62332a
Ragnar Englund <ragnar.englund@annotell.com>
2023-12-22 11:41:43 +0100

    space between lines

--- src/AoC2023/Day2.fs
+++ src/AoC2023/Day2.fs
@@ -5,8 +5,10 @@ module Day2 =
 
   type private Cubes = {red: int; green: int; blue: int}
   type private Game = {id: int; cubes: Cubes seq}
+
   let firstRegex = System.Text.RegularExpressions.Regex(@"Game (\d+):(?:((?:(?: \d+ \w+),)*(?: \d+ \w+))(?:;?))+")
   let secondRegex = System.Text.RegularExpressions.Regex(@"(?: (?:(\d+) (\w+)),?)+")
+
   let private parseGroup group =
       let matches = secondRegex.Matches(group)
       let countSeq = matches.[0].Groups.[1].Captures |> Seq.map (fun c -> c.Value |> int)
@@ -16,6 +18,7 @@ module Day2 =
       let green = Map.tryFind "green" colorMap |> Option.defaultValue 0
       let blue = Map.tryFind "blue" colorMap |> Option.defaultValue 0
       {red = red; green = green; blue = blue}
+
   let private parseGame line =
     let matches = firstRegex.Matches(line)
     let gameNumber = matches.[0].Groups.[1].Captures.[0].Value |> int
@@ -27,11 +30,13 @@ module Day2 =
 
   let private possibleGame cube =
     cube.red <= 12 && cube.green <= 13 && cube.blue <= 14
+
   let part1 input =
     input
     |> List.map parseGame
     |> List.filter (fun g -> Seq.forall possibleGame g.cubes)
     |> List.sumBy (fun g -> g.id)
+
   let private getMaxCubes currentMinCube cube =
     let minRed = max currentMinCube.red cube.red
     let minGreen = max currentMinCube.green cube.green
@@ -47,4 +52,5 @@ module Day2 =
     input
     |> List.map parseGame
     |> List.sumBy getCubePower
+
   let solver = { part1 = part1; part2 = part2}
